37975dcacb4db51b049470c064953079891ff986
omrobbie <putu.roby@yahoo.com>
2018-11-07 08:57:26 +0700

    Initial Commit

---
+++ FirebaseChat Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat Example
+//
+//  Created by omrobbie on 07/11/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
